7216fb38bfee4e1075c26138a02d38d2e1dd5bfe
tarun-upadhyay <tarunu88@gmail.com>
2023-08-23 23:35:05 +0530

    get all url api done

--- controllers/url.controller.js
+++ controllers/url.controller.js
@@ -5,11 +5,32 @@ const URL = require("../Models/url.model");
 const { StatusCodes } = require("http-status-codes");
 
 async function getAllUrl(req, res) {
-  req.body.createdBy = req.user.userEmail;
-  res.send({ msg: "Getallurl" });
+  const { search, sort } = req.query;
+  console.log(search,sort)
+
+  const queryObject = {
+    createdBy: req.user.userEmail,
+  };
+  if (search) {
+    queryObject.title = { $regex: search, $options: "i" };
+  }
+  let result = URL.find(queryObject);
+  if (sort === "latest") {
+    result = result.sort("-createdAt");
+  } else if (sort === "oldest") {
+    result = result.sort("createdAt");
+  } else if (sort === "a-z") {
+    result = result.sort("position");
+  } else if (sort === "z-a") {
+    result = result.sort("-position");
+  }
+
+  const urlData = await result;
+
+  res.status(StatusCodes.OK).json({ urlData });
 }
 async function createShortUrl(req, res) {
-  let { redirectURL } = req.body;
+  const { redirectURL } = req.body;
   if (!redirectURL)
     throw new BadRequestError("Please provide url for shorting");
 
@@ -26,7 +47,7 @@ async function createShortUrl(req, res) {
 
   const urlcreated = await URL.create(req.body);
 
-  res.send({ msg: "create", urlcreated });
+  res.status(StatusCodes.OK).json({ msg: "create", urlcreated });
 }
 
 async function getAnalytics(req, res) {
